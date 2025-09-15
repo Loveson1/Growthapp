f3a0f3c668e3111adbb825e97917eba02241e805
user.name <adeolagodsonomomowo@gmail.com>
2025-09-15 13:21:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// Faq accordion
+document.addEventListener("DOMContentLoaded", () => {
+  const faqContainer = document.querySelector(".faq-content");
+  faqContainer.addEventListener("click", (e) => {
+    const groupHeader = e.target.closest(".faq-group-header");
+    if (!groupHeader) return;
+
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector(".faq-group-body");
+    const icon = groupHeader.querySelector("i");
+
+    //   Toggle Icon
+    icon.classList.toggle("fa-plus");
+    icon.classList.toggle("fa-minus");
+
+    //  Toggle body
+    groupBody.classList.toggle("open");
+
+    // Close other open FAQ bodies
+    const otherGroups = faqContainer.querySelectorAll(".faq-group");
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector(".faq-group-body");
+        const otherIcon = otherGroup.querySelector(".faq-group-header i");
+
+        otherGroupBody.classList.remove("open");
+        otherIcon.classList.remove("fa-minus");
+      }
+    });
+  });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const hamburger = document.querySelector(".hamburger-button")
+  const mobileMenu = document.querySelector(".mobile-menu")
+   const ul = document.querySelector(".mobile-menu ul")
+
+  hamburger.addEventListener("click", ()=> mobileMenu.classList.toggle("active"))
+    ul.addEventListener("click", ()=> mobileMenu.classList.toggle("active"))
+})
